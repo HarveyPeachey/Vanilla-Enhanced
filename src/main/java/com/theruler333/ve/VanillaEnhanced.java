2f1d93feef648bf217325ad57912a368153028de
Theruler333 <harveypeachey@hotmail.co.uk>
2014-07-29 18:19:11 +0100

    Starting Off

---
+++ src/main/java/com/theruler333/ve/VanillaEnhanced.java
@@ -0,0 +1,35 @@
+package com.theruler333.ve;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="VanillaEnhanced", name="Vanilla Enhanced", version="1.7.2-1.0")
+
+public class VanillaEnhanced
+{
+
+    @Mod.Instance
+    public static VanillaEnhanced instance;
+
+    @Mod.EventHandler
+    public void preInt(FMLPreInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void preInt(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void preInt(FMLPostInitializationEvent event)
+    {
+
+    }
+
+
+
+}
